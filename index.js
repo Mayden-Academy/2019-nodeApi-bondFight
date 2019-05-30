478f4cc9009bdc07ab684399f2a2cfb5339ea699
Alessandro Corra <alecorra1980@gmail.com>
2019-05-30 14:18:05 +0100

    put route created, checked with postman, variables loserId and winnerId extracted

--- index.js
+++ index.js
@@ -2,36 +2,57 @@ const express = require('express')
 const MongoClient = require('mongodb').MongoClient
 const app = express()
 const port = 3001
+const bodyParser = require('body-parser')
+const jsonParser = bodyParser.json()
+const ObjectId = require('mongodb').ObjectId
 
 const url = 'mongodb://localhost:27017'
 const dbname = 'Films'
 const Client = new MongoClient(url, { useNewUrlParser: true})
 
-app.put('/bondFilms/', function (req, res) {
-    Client.connect(function (err) {
-        let db = Client.db(dbname)
-        let cardId1 = req.body.cardId1
-        let cardId2 = req.body.cardId2
-        updateDataInDb(db, filmObject, function (result) {
-            if(result.modifiedCount){
-                res.json(success: true,
-                    msg: 'successfully completed task',
-                    data: [])
-            } else {
-                res.json(success: false,
-                    msg: 'whoops',
-                    data: [])
+var updateWinner = function(db, winnerId, loserId, callback) {
+    var collection = db.collection('Bond_Films')
+    collection.updateOne({'_id' : winnerId},
+        {$inc: {'favourite' : 1, 'appeared': 1}}, function(err, result) {
+            if(result.modifiedCount) {
+                updateLoser(collection, loserId, callback)
             }
         })
-    })
-})
-
-var updateDataInDb = function(db, filmObject, callback) {
-    var collection = db.collection('Bond_Films');
-    collection.updateOne({ "" : filmObject }
-        , { $set: { "completed" : 1 } }, function(err, result) {
-            callback(result)
-        });
 }
 
-// $inc: {appeared: ammount1}
+var updateLoser = function (collection, loserId, callback) {
+    collection.updateOne({ "_id" : loserId },
+        { $inc: { "appeared" : 1 } }, function(err, result) {
+            if(result.modifiedCount) {
+                callback(result.modifiedCount)
+            }
+        })
+}
+
+app.put('/bondFilms/', jsonParser, function (req, res) {
+    Client.connect(function (err) {
+        let db = Client.db(dbname)
+        let loserId = ObjectId(req.body.loserId)
+        let winnerId = ObjectId(req.body.winnerId)
+
+        try {
+            updateWinner(db, winnerId, loserId, function (modifiedCount) {
+                if (modifiedCount) {
+                    res.json({
+                        success: true,
+                        msg: 'successfully completed task',
+                        data: []
+                    })
+                }
+            })
+        } catch (error) {
+            res.json({
+                success: false,
+                msg: error,
+                data: []
+            })
+        }
+
+    })
+
+app.listen(port, ()=> console.log('bond database running'))
