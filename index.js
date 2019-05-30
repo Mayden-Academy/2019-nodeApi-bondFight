ab508f93d35b6cca9785130dd2e64a5345c44145
Alessandro Corra <alecorra1980@gmail.com>
2019-05-30 14:44:38 +0100

    indentation sorted

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const MongoClient = require('mongodb').MongoClient
 const app = express()
 const port = 3001
+
 const bodyParser = require('body-parser')
 const jsonParser = bodyParser.json()
 const ObjectId = require('mongodb').ObjectId
@@ -54,5 +55,6 @@ app.put('/bondFilms/', jsonParser, function (req, res) {
         }
 
     })
+})
 
 app.listen(port, ()=> console.log('bond database running'))
