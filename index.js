87b071d8858fc1b8e95410092ace4a11bdb9673e
Knux2 <ben_ashfield@hotmail.com>
2019-05-31 11:48:06 +0100

    Removed duplicate code

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ const port = 3001
 const bodyParser = require('body-parser')
 const jsonParser = bodyParser.json()
 const ObjectId = require('mongodb').ObjectId
-
-const bodyParser = require('body-parser')
-const jsonParser = bodyParser.json()
-const ObjectId = require('mongodb').ObjectId
 const url = 'mongodb://localhost:27017'
 const dbname = 'Films'
 const Client = new MongoClient(url, { useNewUrlParser: true})
@@ -34,6 +30,13 @@ var updateLoser = function (collection, loserId, callback) {
                 callback(result.modifiedCount)
             }
         })
+}
+
+const getAllFilms = function (db, callback) {
+    var collection = db.collection('Bond_Films')
+    collection.find().toArray(function (err, documents) {
+        callback(documents)
+    })
 }
 
 app.put('/Bond_Films', jsonParser, function (req, res) {
@@ -80,58 +83,6 @@ app.get('/Bond_Films', function (req, res) {
         } catch (error){
             response.message = error
             res.json(response)
-        }
-    })
-})
-
-const getAllFilms = function (db, callback) {
-    var collection = db.collection('Bond_Films')
-    collection.find().toArray(function (err, documents) {
-        callback(documents)
-    })
-}
-
-var updateWinner = function(db, winnerId, loserId, callback) {
-    var collection = db.collection('Bond_Films')
-    collection.updateOne({'_id' : winnerId},
-        {$inc: {'favourite' : 1, 'appeared': 1}}, function(err, result) {
-            if(result.modifiedCount) {
-                updateLoser(collection, loserId, callback)
-            }
-        })
-}
-
-var updateLoser = function (collection, loserId, callback) {
-    collection.updateOne({ "_id" : loserId },
-        { $inc: { "appeared" : 1 } }, function(err, result) {
-            if(result.modifiedCount) {
-                callback(result.modifiedCount)
-            }
-        })
-}
-
-app.put('/bondFilms/', jsonParser, function (req, res) {
-    Client.connect(function (err) {
-        let db = Client.db(dbname)
-        let loserId = ObjectId(req.body.loserId)
-        let winnerId = ObjectId(req.body.winnerId)
-
-        try {
-            updateWinner(db, winnerId, loserId, function (modifiedCount) {
-                if (modifiedCount) {
-                    res.json({
-                        success: true,
-                        msg: 'successfully completed task',
-                        data: []
-                    })
-                }
-            })
-        } catch (error) {
-            res.json({
-                success: false,
-                msg: error,
-                data: []
-            })
         }
     })
 })
